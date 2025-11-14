7363a6d9d1613c11fffc7893952e19c46359c8a5
pnku <159317511+pnk2u@users.noreply.github.com>
2025-11-14 05:09:20 +0100

    Fix/Improve ~Menu Mixin (1.8.2|3)

--- src/main/java/de/pnku/mcgt/mixin/CartographyTableMenuMixin.java
+++ src/main/java/de/pnku/mcgt/mixin/CartographyTableMenuMixin.java
@@ -1,27 +1,23 @@
 package de.pnku.mcgt.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import de.pnku.mcgt.block.MoreCartographyTablesBlock;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.CartographyTableMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CartographyTableMenu.class)
 public abstract class CartographyTableMenuMixin {
 
-    @Shadow
-    @Final
-    private ContainerLevelAccess access;
-
-    @Inject(method = "stillValid", at = @At("HEAD"), cancellable = true)
-    private void canUse(Player player, CallbackInfoReturnable<Boolean> info) {
-        if (this.access.evaluate((world, pos) -> world.getBlockState(pos).getBlock() instanceof MoreCartographyTablesBlock, true)) {
-            info.setReturnValue(true);
+    @WrapOperation(method = "stillValid", at = @At(value="INVOKE", target="Lnet/minecraft/world/inventory/CartographyTableMenu;stillValid(Lnet/minecraft/world/inventory/ContainerLevelAccess;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/level/block/Block;)Z"))
+    private boolean wrappedStillValid(ContainerLevelAccess containerLevelAccess, Player player, Block block, Operation<Boolean> original) {
+        if (containerLevelAccess.evaluate((world, pos) -> world.getBlockState(pos).getBlock() instanceof MoreCartographyTablesBlock, true)) {
+            return true;
         }
+        return original.call(containerLevelAccess, player, block);
     }
 }
