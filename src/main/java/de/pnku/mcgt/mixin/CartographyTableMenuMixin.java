fc9f6d34234fb973cafd85c5ac58d8c64d31d0c8
pnku <159317511+pnk2u@users.noreply.github.com>
2025-11-14 05:04:16 +0100

    Rename Mixin (yarn->mojmaps) (1.8.2|2)

--- src/main/java/de/pnku/mcgt/mixin/CartographyTableMenuMixin.java
+++ src/main/java/de/pnku/mcgt/mixin/CartographyTableMenuMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(CartographyTableMenu.class)
-public abstract class CartographyTableScreenMixin {
+public abstract class CartographyTableMenuMixin {
 
     @Shadow
     @Final
